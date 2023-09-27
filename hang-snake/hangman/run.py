4ec96495be9408f024ab23678064d3dbb7e30717
Александр <nikol.alex06@mail.ru>
2023-09-27 14:06:38 +0300

    Snake without repeat

--- hang-snake/hangman/run.py
+++ hang-snake/hangman/run.py
@@ -1,33 +1,66 @@
 from common.util import clear_terminal
+from pynput import keyboard
 
+secret_word = input('enter secret word').lower()
 
-def create_secret():
-    return 'capybara'
-
-
-SECRET = create_secret()
-n = len(SECRET)
-
-FINAL_FIELD = r'''
+lose_field = r'''
    +----+
    |    |
    o    |
   /|\   |
   / \   |
 _______/|\_
-'''
+'''.split('\n')
+loser = [
+    (3, 3),
+    (4, 3),
+    (4, 2),
+    (4, 4),
+    (5, 2),
+    (5, 4)
+]
+last_letter = []
+picture = [list(row) for row in lose_field]
+for i in loser:
+    picture[i[0]][i[1]] = ' '
 
-# здесь мы наверное хотим иметь исходное поле
-# и понимание, как оно меняется после каждого хода
-FIELD = FINAL_FIELD
+part_player = 0
+
+n = len(secret_word)
+enter_field = ['_' for _ in range(n)]
 
 while True:
-    # make a move!
-    letter = input('Enter your guess: ')
-    if ...:
-        FIELD = ...  # если не угадали, то надо обновить поле
-    else:
-        ...  # мало ли, понадобится...
-
     clear_terminal()
-    print(FIELD)
+    for row in picture:
+        print(''.join(row))
+    print()
+    print()
+    print(''.join(enter_field))
+    letter = input('enter letter').lower()
+    if ord(letter) < ord('a') or ord(letter) > ord('z') and len(letter) != 1:
+        print('Error of the enter')
+        continue
+    elif letter in last_letter:
+        print('This letter was already entered. Try other.')
+        continue
+    if letter in secret_word:
+        for i in range(n):
+            if secret_word[i] == letter:
+                enter_field[i] = letter
+    else:
+        part = loser[part_player]
+        picture[part[0]][part[1]] = lose_field[part[0]][part[1]]
+        part_player += 1
+
+    last_letter.append(letter)
+
+    if len(loser) == part_player:
+        print('You lose! Congratulations!')
+        print()
+        print('If you want to try again tap to space')
+        break
+    elif '_' not in enter_field:
+        print('You won! Congratulations!')
+        print()
+        print('If you want to try again tap to escape')
+        break
