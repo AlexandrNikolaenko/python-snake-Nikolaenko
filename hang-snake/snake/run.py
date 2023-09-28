7a0e528acddb0a34ccf2655d2837bbf725dd17a2
Александр <nikol.alex06@mail.ru>
2023-09-29 00:17:20 +0300

    Not full snake

--- hang-snake/snake/run.py
+++ hang-snake/snake/run.py
@@ -1,14 +1,14 @@
+from common.util import clear_terminal
 from pynput import keyboard
 from random import randint
+from array import *
 
-WIDTH, HEIGHT = ..., ...
-# можно приделать конфиг-файл с параметрами
+width, height = 20, 20
+
 direction = (1, 0)
 
-
-def random_position():
-    return randint(0, HEIGHT - 1), randint(0, WIDTH - 1)
-
+def generate_position():
+    return randint(0, width - 1), randint(0, height - 1)
 
 def process_press(key):
     # обработчик нажатия на клавиши (можно сделать и поаккуратнее)
@@ -23,14 +23,40 @@ def process_press(key):
         case keyboard.Key.down:
             direction = (1, 0)
 
+snake = (i, [[generate_position()]])
+apple = generate_position()
+body_snake = 0
+new_elem = (snake[0][0] + direction[0], snake[0][1] + direction[1])
 
-snake = [random_position()]
-apple = random_position()
 while apple in snake:
-    apple = random_position()
+    apple = generate_position()
 
-# оно умеет мониторить нажатия на кнопки!
 with keyboard.Listener(on_press=process_press) as listener:
     while True:
-        # let's play the game!
-        pass
+        clear_terminal()
+        # рисуем змею и яблоко на поле
+        field = [['.' for i in range(width)] for i in range(height)]
+        for i in body_snake:
+            if i == 0:
+                field[snake[0][0]][snake[0][1]] = 'O'
+            else:
+                field[snake[i][0]][snake[i][1]] = 'o'
+        field[apple[0]][apple[1]] = 'a'
+
+        # выводим картинку
+
+        for row in field:
+            print(''.join(row))
+
+        # передвигаем змею
+
+        snake.insert(0, new_elem)
+
+        if new_elem == apple:
+            while apple in snake:
+                apple = generate_position()
+        else:
+            snake.pop(-1)
+
+
+
